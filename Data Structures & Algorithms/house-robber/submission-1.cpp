88735312d096da528ebca923dbcc4aaef1eb5a6c
Shreya <272812472+mallikshreya509@users.noreply.github.com>
2026-06-03 18:52:39 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+vector<int>dp;
+
+ //dp[i] = maximum money that can be robbed starting from index i
+  
+  int recur(vector<int>&dp , vector<int>&nums,int ind){
+
+    int n=nums.size();
+    if(ind>=n) return 0;
+    if(dp[ind]!=-1) return dp[ind];
+    int notpick=recur(dp,nums,ind+1);
+    int pick=recur(dp,nums,ind+2)+nums[ind];
+
+    return dp[ind]= max(notpick,pick);
+  }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        dp.resize(n,-1);
+        return recur(dp,nums,0); 
+    }
+};
